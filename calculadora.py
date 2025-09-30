8576baf86adb62970b7c57d98209c713f8cc32e0
martinalejandroo25 <martinalejandroo25@gmail.com>
2025-09-30 20:10:20 +0200

    Añadir fecha y descripción del sistema

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,13 @@
-#Calculadora colaborativa
-
-#Proyecto de calculadora desarrollado en equipo
+# Calculadora Colaborativa
+"""
+Proyecto de calculadora desarrollado en equipo
+Fecha: 2024
+"""
 
 def main():
     print("Calculadora v1.0")
-    print("Desarrollada por: Juan Antonio y Martín")
+    print("Desarrollada por: Alumno A y Alumno B")
+    print("Sistema de cálculo matemático")
 
 if __name__ == "__main__":
     main()
-    
